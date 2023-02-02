64ff18fad5936fbbe072aed4ebc6ade734b96cca
billonium4535 <88295937+billonium4535@users.noreply.github.com>
2023-02-02 13:15:51 +0000

    Add files via upload

--- index_script.js
+++ index_script.js
@@ -1,5 +1,9 @@
 var countDownDate = new Date("May 29, 2023 18:00:00").getTime();
 
+function pad(d) {
+    return (d < 10) ? '0' + d.toString() : d.toString();
+}
+
 var x = setInterval(function() {
 	var now = new Date().getTime();
 	var distance = countDownDate - now;
@@ -8,7 +12,7 @@ var x = setInterval(function() {
 	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
-	document.getElementById("timer").innerHTML = days + ":" + hours + ":" + minutes + ":" + seconds;
+	document.getElementById("timer").innerHTML = pad(days) + ":" + pad(hours) + ":" + pad(minutes) + ":" + pad(seconds);
     
 	if (distance < 0) {
 		clearInterval(x);
